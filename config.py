67a631b971dd750dc01cce0083cbdd79574f3a42
ghalidouga <ghalighalidouga@gmail.com>
2019-08-11 12:52:46 +0700

    adding a config file

---
+++ config.py
@@ -0,0 +1,7 @@
+email = 'youEmailHere@example.com'
+password = 'yourPasswordHere'
+topic = 'yourTopicHere'
+delay = 10
+
+
+# Delay means a time delay between each auto like and retweet in a seconds. Don't make it too quick because Twitter might be suspend your account
